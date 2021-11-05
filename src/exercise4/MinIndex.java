106f78b1716fab6e79ccdb2e1b330c3b84dac831
Stanislav Stefanov <stanislav.d.stefanov@fdiba.tu-sofia.bg>
2021-11-05 19:42:26 +0200

    Update MinIndex.java

--- src/exercise4/MinIndex.java
+++ src/exercise4/MinIndex.java
@@ -8,7 +8,7 @@ public class MinIndex {
     Scanner sc = new Scanner(System.in);
     int length = sc.nextInt();
     double[] arr = new double[length];
-    double min = 100;
+    double min = 100000;
 
     for(int i = 0; i < arr.length; i++) {
       arr[i] = sc.nextDouble();
